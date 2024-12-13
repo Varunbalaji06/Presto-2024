a8e18d8606923074e5582cd72ca718e509b20235
Varunbalaji06 <varunbalaji06@gmail.com>
2024-12-13 23:54:01 +0530

    Initial Commit

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
-    presets: ['babel-preset-expo'],
+    presets: [
+      'module:metro-react-native-babel-preset',
+      'babel-preset-expo'
+    ],
     plugins: ['nativewind/babel'],
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/app/signin.{js,jsx,ts,tsx}",
-    "./src/app/signup_email.{js,jsx,ts,tsx}",
-    "./src/app/signup_num.{js,jsx,ts,tsx}",
-    "./src/app/welcome.{js,jsx,ts,tsx}",
-    "./src/cart/**/*.{js,jsx,ts,tsx}",
-    "./src/menu/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
