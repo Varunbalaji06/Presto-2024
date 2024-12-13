dfe41261b1ff78e814c2d78df642fcb631088ed4
Varunbalaji06 <varunbalaji06@gmail.com>
2024-12-13 21:22:44 +0530

    Final UI Completed File

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/app/signin.{js,jsx,ts,tsx}",
+    "./src/app/signup_email.{js,jsx,ts,tsx}",
+    "./src/app/signup_num.{js,jsx,ts,tsx}",
+    "./src/app/welcome.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
